d1d906481f26c4801486a4da807fda061d7c211c
Ferenc Dajka <fesath_3@hotmail.com>
2020-08-19 14:54:37 +0200

    !A Paw weapon

---
+++ wolf-and-stein-unity-project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    Animator animator;
+    float Speed = 0;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            Speed = 1;
+            animator.SetFloat("Speed", Speed);
+		}   
+    }
+
+    public void OnReachAnimEnd()
+	{
+        Speed = 0;
+        animator.SetFloat("Speed", Speed);
+    }
+}
