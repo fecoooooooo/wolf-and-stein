409148798ee28292c690c53127355bf5815ffee7
Dajka Ferenc <dajka1989@gmail.com>
2020-10-09 11:25:44 +0200

    !WIP Death effect

--- wolf-and-stein-unity-project/Assets/Scripts/ScreenEffect.cs
+++ wolf-and-stein-unity-project/Assets/Scripts/ScreenEffect.cs
@@ -5,37 +5,48 @@ using UnityEngine.UI;
 
 public class ScreenEffect : MonoBehaviourSingleton<ScreenEffect>
 {
-    public float AnimationHalfTime = .05f;
-    public float GoalOpacity = .75f;
+    public float BlinkAnimationHalfTime = .05f;
+    public float BlinkGoalOpacity = .75f;
+
+    public float DeathAnimTime = 1f;
+    public int DeathAnimIterationsPerFrame = 30;
 
     Image powerUpEffectImg;
     Image damageEffectImg;
+    RawImage deathEffectImg;
 
 	void Start()
     {
         powerUpEffectImg = transform.Find("PowerUpEffectImg").GetComponent<Image>();
         damageEffectImg = transform.Find("DamageEffectImg").GetComponent<Image>();
+        deathEffectImg = transform.Find("DeathEffectImg").GetComponent<RawImage>();
     }
 
     public void PlayPowerUp()
 	{
-        StartCoroutine(PlayEffectOnImage(powerUpEffectImg));
+        StartCoroutine(PlayBlinkEffectOnImage(powerUpEffectImg));
 	}
 
     public void PlayDamage()
     {
-        StartCoroutine(PlayEffectOnImage(damageEffectImg));
+        StartCoroutine(PlayBlinkEffectOnImage(damageEffectImg));
     }
 
-    IEnumerator PlayEffectOnImage(Image image)
+    public void PlayDeath()
+	{
+        StartCoroutine(PlayPixelizeOnTexture(deathEffectImg));
+	}
+
+
+    IEnumerator PlayBlinkEffectOnImage(Image image)
 	{
         float timePassed = 0;
         Color currentColor = image.color;
 
-        while(timePassed < AnimationHalfTime)
+        while(timePassed < BlinkAnimationHalfTime)
 		{
-            float t = timePassed / AnimationHalfTime;
-            currentColor.a = Mathf.Lerp(0, GoalOpacity, t);
+            float t = timePassed / BlinkAnimationHalfTime;
+            currentColor.a = Mathf.Lerp(0, BlinkGoalOpacity, t);
             image.color = currentColor;
             
             timePassed += Time.deltaTime;
@@ -45,10 +56,10 @@ public class ScreenEffect : MonoBehaviourSingleton<ScreenEffect>
 
         timePassed = 0;
 
-        while (timePassed < AnimationHalfTime)
+        while (timePassed < BlinkAnimationHalfTime)
         {
-            float t = timePassed / AnimationHalfTime;
-            currentColor.a = Mathf.Lerp(GoalOpacity, 0, t);
+            float t = timePassed / BlinkAnimationHalfTime;
+            currentColor.a = Mathf.Lerp(BlinkGoalOpacity, 0, t);
             image.color = currentColor;
 
             timePassed += Time.deltaTime;
@@ -59,4 +70,30 @@ public class ScreenEffect : MonoBehaviourSingleton<ScreenEffect>
         currentColor.a = 0;
         image.color = currentColor;
     }
+
+    IEnumerator PlayPixelizeOnTexture(RawImage image)
+	{
+        int width = (int)image.rectTransform.rect.width;
+        int height = (int)image.rectTransform.rect.height;
+
+        Texture2D texture = new Texture2D(width / 5, height / 5);
+        image.texture = texture;
+        
+        float timePassed = 0;
+        while(timePassed < DeathAnimTime)
+		{
+            for(int i = 0; i < DeathAnimIterationsPerFrame; ++i)
+			{
+                int u = Random.Range(0, texture.width);
+                int v = Random.Range(0, texture.height);
+                texture.SetPixel(u, v, Color.red);
+			}
+
+            texture.Apply();
+
+            timePassed += Time.deltaTime;
+            yield return null;
+		}
+	}
+
 }
